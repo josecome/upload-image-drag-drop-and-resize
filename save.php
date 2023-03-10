be2378b03abf80c602a6135bff5d9db01f77840c
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-10 19:24:24 +0200

    PHP functions added

--- save.php
+++ save.php
@@ -1,4 +1,17 @@
 <?php 
+function save($a, $b) {
+    try {
+        $f = fopen( $b, 'wb' ); 
+        $d = explode( ',', $a );
+        fwrite( $f, base64_decode( $d[ 1 ] ) );
+        fclose( $f ); 
+        return "Image saved!"; 
+    }catch(Exception $e) {
+        return "Error ocurred!";
+    }
 
+}
+
+echo save($_POST['file'], 'uploaded_images/' . $_POST['name']);
 
 ?>
